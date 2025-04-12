3c28110d07e3ff6a68f8b87beeaa60b38f958c58
Chitresh8 <chitreshbabu8@gmail.com>
2025-04-12 15:26:26 +0530

    Fourth commit

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ class App extends Component {
       <>
         <h1>Vite + React</h1>
         <div className="card">
-          <h1>Hello, my name is {this.state.name}, I work at {this.state.company}</h1>
+          <h1>Hello, My Name is {this.state.name}, I work at {this.state.company}</h1>
           <button onClick={()=>{this.setState({name:'Andrei'})}}>Change Name</button>
         </div>
       </>
